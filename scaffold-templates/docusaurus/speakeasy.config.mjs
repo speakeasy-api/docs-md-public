b06769939cfa075b8b81c0b6f8c6044139372a91
Bryan Hughes <bryan.hughes@speakeasy.com>
2025-06-11 14:12:00 -0700

    Scaffold template (#17)
    * Created base scaffold
    
    * Added Nextra scaffold

---
+++ scaffold-templates/docusaurus/speakeasy.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  spec: "./petstore.yaml",
+  output: {
+    pageOutDir: "./docs/api",
+    componentOutDir: "./src/components/speakeasy",
+    framework: "docusaurus",
+  },
+};
---
+++ scaffold-templates/nextra/speakeasy.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  spec: "./petstore.yaml",
+  output: {
+    pageOutDir: "./src/app/api",
+    componentOutDir: "./src/components/speakeasy",
+    framework: "nextra",
+  },
+};
