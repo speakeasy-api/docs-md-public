0031edef622d3cc9d56cf41a5006d7692a5ad680
nebrius <bryan.hughes@speakeasy.com>
2025-06-16 12:18:47 -0700

    Path fix

--- packages/docs-md/src/index.ts
+++ packages/docs-md/src/index.ts
@@ -1,3 +1,3 @@
 export type { Settings } from "./types/settings.ts";
 export { TryItNow } from "./components/TryItNow/index.tsx";
-export { SnippetAI } from "./components/SnippetAI/index.tsx";
+export { SnippetAI } from "./components/SnippetAI/index.js";
