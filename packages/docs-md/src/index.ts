a0174d78add4bcac7c16f211ddbdc0a710c5f756
Bryan Hughes <bryan.hughes@speakeasy.com>
2025-07-01 16:14:35 -0700

    Removed custom site stuff for now since it breaks Docusaurus builds (#34)

--- packages/docs-md/src/index.ts
+++ packages/docs-md/src/index.ts
@@ -4,11 +4,3 @@ export type { Settings } from "./types/settings.ts";
 // Components used in rendered docs
 export { TryItNow } from "./components/TryItNow/index.tsx";
 export { SideBar, SideBarTrigger } from "./components/SideBar/index.tsx";
-
-// Stuff needed for custom renderers
-export { Site } from "./renderers/base/site.ts";
-export { Renderer } from "./renderers/base/renderer.ts";
-export { MarkdownSite } from "./renderers/base/markdown.ts";
-export { MarkdownRenderer } from "./renderers/base/markdown.ts";
-export { MdxSite } from "./renderers/base/mdx.ts";
-export { MdxRenderer } from "./renderers/base/mdx.ts";
