56814516700e0641f4788c44ec7875e091d876f7
Bryan Hughes <bryan.hughes@speakeasy.com>
2025-07-18 17:14:30 -0700

    Got Nextra basically working (#54)

--- packages/docs-md/src/components/Section/nextra/Section.tsx
+++ packages/docs-md/src/components/Section/nextra/Section.tsx
@@ -1,17 +1,32 @@
+import clsx from "clsx";
+
 import type { SectionProps } from "../common/types.ts";
 import { useChildren, useUniqueChild } from "../hooks.ts";
 import styles from "./styles.module.css";
 
-export function NextraSection({ children }: SectionProps) {
+export function NextraSection({
+  children,
+  contentBorderVariant,
+  noTopBorderRadius,
+}: SectionProps) {
   const titleChild = useUniqueChild(children, "title");
   const contentChildren = useChildren(children, "content");
 
+  if (noTopBorderRadius !== undefined) {
+    console.log("noTopBorderRadius", noTopBorderRadius);
+  }
+
   return (
     <div className={styles.section}>
-      {/* Wrap these in divs to isolate CSS to make sure that :first-child in
-          content is applied to the first content child, not the title */}
       <div>{titleChild}</div>
-      <div>{contentChildren}</div>
+      <div
+        className={clsx(
+          contentBorderVariant === "all" && styles.contentBorderAll,
+          noTopBorderRadius && styles.noTopBorderRadius
+        )}
+      >
+        {contentChildren}
+      </div>
     </div>
   );
 }
