23325cdcf9f593b689f15893441d9eed9b236477
Bryan Hughes <bryan.hughes@speakeasy.com>
2025-10-07 12:17:40 -0700

    Set docs data in editor entry points (#152)

--- packages/compiler/src/editor/editor.ts
+++ packages/compiler/src/editor/editor.ts
@@ -1,4 +1,4 @@
-import type { OperationChunk } from "@speakeasy-api/docs-md-shared";
+import type { Chunk, OperationChunk } from "@speakeasy-api/docs-md-shared";
 
 export type { Settings } from "../settings.ts";
 export type { FrameworkConfig } from "../types/FrameworkConfig.ts";
@@ -15,48 +15,56 @@ import { setSettings } from "../settings.ts";
 import type { FrameworkConfig } from "../types/FrameworkConfig.ts";
 
 export function renderSecurity(
+  docsData: Map<string, Chunk>,
   security: OperationChunk,
   settings: Settings,
   frameworkConfig: FrameworkConfig
 ) {
   setSettings(settings);
   const site = new MdxSite(frameworkConfig);
+  site.setDocsData(docsData);
   const renderer = site.createPage("");
   renderSecurityInternal(security.chunkData.security, renderer);
   return renderer.render().contents;
 }
 
 export function renderParameters(
+  docsData: Map<string, Chunk>,
   operationChunk: OperationChunk,
   settings: Settings,
   frameworkConfig: FrameworkConfig
 ) {
   setSettings(settings);
   const site = new MdxSite(frameworkConfig);
+  site.setDocsData(docsData);
   const renderer = site.createPage("");
   renderParametersInternal(operationChunk.chunkData.parameters, renderer);
   return renderer.render().contents;
 }
 
 export function renderRequestBody(
+  docsData: Map<string, Chunk>,
   operationChunk: OperationChunk,
   settings: Settings,
   frameworkConfig: FrameworkConfig
 ) {
   setSettings(settings);
   const site = new MdxSite(frameworkConfig);
+  site.setDocsData(docsData);
   const renderer = site.createPage("");
   renderRequestBodyInternal(operationChunk.chunkData.requestBody, renderer);
   return renderer.render().contents;
 }
 
 export function renderResponseBodies(
+  docsData: Map<string, Chunk>,
   operationChunk: OperationChunk,
   settings: Settings,
   frameworkConfig: FrameworkConfig
 ) {
   setSettings(settings);
   const site = new MdxSite(frameworkConfig);
+  site.setDocsData(docsData);
   const renderer = site.createPage("");
   renderResponseBodiesInternal(operationChunk.chunkData.responses, renderer);
   return renderer.render().contents;
