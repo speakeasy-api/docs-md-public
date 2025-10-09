fb6de00baa9f502c99cff5331c258bc9b8b02c8d
Jesus Garnica <jesus@speakeasy.com>
2025-10-09 15:10:45 -0700

    fix: TIN adjust breakpoints and remove CSS nesting (#162)
    * make breakpoint bigger, reduce redundant class container
    
    * fix broken token
    
    * undo nesting to avoid build errors

--- src/components/TryItNow/components/RunButton.tsx
+++ src/components/TryItNow/components/RunButton.tsx
@@ -5,15 +5,13 @@ import styles from "./styles.module.css";
 
 export function RunButton({ onClick }: RunButtonProps) {
   return (
-    <div className={styles.runButtonContainer}>
-      <button
-        onClick={onClick}
-        type="button"
-        aria-label="Run code"
-        className={styles.runButton}
-      >
-        Run
-      </button>
-    </div>
+    <button
+      onClick={onClick}
+      type="button"
+      aria-label="Run code"
+      className={styles.runButton}
+    >
+      Run
+    </button>
   );
 }
